9730621748f0cb32bc89526d760731298a294aa1
balajiNadar08 <balajigittttt@gmail.com>
2026-06-25 00:50:49 +0530

    Added few text

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -18,27 +18,39 @@ export default function BlogsPage() {
   const blogs = getAllBlogs();
 
   return (
-    <div className={`min-h-screen bg-[#F3E4C9] ${specialElite.className} pb-10`}>
+    <div
+      className={`min-h-screen bg-[#F3E4C9] ${specialElite.className} pb-10`}
+    >
       <Header title="Balaji's Blogs" />
 
       <div className="max-w-6xl mx-auto px-6">
         <div className="flex flex-col md:flex-row gap-28">
           <SidebarFullScreen />
 
-          <div className="flex flex-col gap-4">
+          <div className="flex-1 flex-col gap-4">
+            <div className="mb-12 text-center">
+              <h1 className="mb-4 text-5xl font-bold">
+                Know What's Going On in My Life
+              </h1>
+
+              <p className="text-lg">
+                Projects I'm building, things I'm learning, books I'm reading
+                and thoughts worth sharing.
+              </p>
+            </div>
+
             {blogs.map((blog) => (
-            <BlogCard
-              key={blog.slug}
-              title={blog.title}
-              description={blog.description}
-              date={blog.date}
-              coverImage={blog.coverImage}
-              tags={blog.tags}
-              slug={blog.slug}
-            />
-          ))}
+              <BlogCard
+                key={blog.slug}
+                title={blog.title}
+                description={blog.description}
+                date={blog.date}
+                coverImage={blog.coverImage}
+                tags={blog.tags}
+                slug={blog.slug}
+              />
+            ))}
           </div>
-          
         </div>
       </div>
     </div>
